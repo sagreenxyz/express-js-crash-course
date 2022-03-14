fbc8af521b8e0157d00e138ca2052d94e216888b
Scott Green <sagreenxyz@gmail.com>
2022-03-14 15:32:30 -0500

    moving members data to separate file

--- index.js
+++ index.js
@@ -5,26 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const members = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Bob Williams',
-        email: 'bob@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 3,
-        name: 'Shannon Jackson',
-        email: 'shannon@gmail.com',
-        status: 'active'
-    }
-]
+const members = require('./Members');
 
 // ROUTE HANDLERS
 
