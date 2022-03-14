a17e0be7111301489e89fdfecc8ca01c2c8ae0e0
Scott Green <sagreenxyz@gmail.com>
2022-03-14 15:28:26 -0500

    more functionality...

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+]
+
 // ROUTE HANDLERS
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/members', (req, res) => {
+
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}; http://localhost:${PORT}`)
