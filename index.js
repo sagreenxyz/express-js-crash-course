6537635cbe55c20fb658d369c6f1b31a358c9bca
Scott Green <sagreenxyz@gmail.com>
2022-03-14 15:40:26 -0500

    moving logger middleware to separate file

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const path = require('path');
-const moment = require('moment');
-
+const logger = require('./middleware/logger')
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 const members = require('./Members');
 
-const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
-    next();
-};
+
 
 // MIDDLEWARE
 app.use(logger);
