f0a9070f5010581056fd920a763159a6452e179f
Scott Green <sagreenxyz@gmail.com>
2022-03-14 15:52:18 -0500

    handing bad request

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ app.get('/api/members', (req, res) => {
 });
 
 app.get('/api/members/:id', (req, res) => {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if(found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `No member with id of ${req.params.id}`}); // bad request
+    }
 });
 
 app.listen(PORT, () => {
