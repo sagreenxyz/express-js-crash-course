22ebb66407e7c022ce5680b406808dabba918816
Scott Green <sagreenxyz@gmail.com>
2022-03-14 16:24:32 -0500

    delete route to members api

--- routes/api/members.js
+++ routes/api/members.js
@@ -46,8 +46,20 @@ router.put('/:id', (req, res) => {
             if(member.id === parseInt(req.params.id)) {
                 member.name = updMember.name ? updMember.name : member.name;
                 member.email = updMember.email ? updMember.email : member.email;
+
+                res.json({msg: 'Member updated', member});
             };
         });
+    } else {
+        res.status(400).json({ msg: `No member with id of ${req.params.id}` }); // bad request
+    }
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found) {
+        res.json({msg: 'Member deleted', members: members.filter(member => member.id !== parseInt(req.params.id))});
     } else {
         res.status(400).json({ msg: `No member with id of ${req.params.id}` }); // bad request
     }
